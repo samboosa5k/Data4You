32f29173acd9818bce2a57aa25c7bd268703a1cf
Jasper Verbon <jasper.verbon@gmail.com>
2019-09-12 20:59:23 +0200

    created html/css layouts for playtime

--- projects/portfolio_prototype_2/script.js
+++ projects/portfolio_prototype_2/script.js
@@ -1,3 +1,9 @@
+function getNewHeight() {
+    var height = document.documentElement.clientHeight;
+    var firstArticle = document.getElementsByClassName( "article__home" );
+    console.log( height );
+}
+
 function openmenu() {
     var menu = document.getElementById( "menu_id" );
     menu.classList.toggle( "hidden" );
