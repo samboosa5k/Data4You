daa4758771bd8a326b5046fc59f6742484e08590
samboosa5k <43879542+samboosa5k@users.noreply.github.com>
2019-09-13 09:46:09 +0200

    Simple product launch single page exercise

--- projects/portfolio_prototype_2/script.js
+++ projects/portfolio_prototype_2/script.js
@@ -1,7 +1,10 @@
 function getNewHeight() {
     var height = document.documentElement.clientHeight;
-    var firstArticle = document.getElementsByClassName( "article__home" );
+    var firstArticle = document.getElementsByClassName( "article__home" )[0];
+    var newHeight = height - 64 + "px";
+    firstArticle.style.height = newHeight;
     console.log( height );
+    console.log( newHeight );
 }
 
 function openmenu() {
