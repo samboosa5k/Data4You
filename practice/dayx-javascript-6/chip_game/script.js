fef81e3bf6b96db19e1b990dec226eb68cf0bfa2
samboosa5k <43879542+samboosa5k@users.noreply.github.com>
2019-10-02 14:56:59 +0200

    Refactored chip game code

--- practice/dayx-javascript-6/chip_game/script.js
+++ practice/dayx-javascript-6/chip_game/script.js
@@ -1,32 +1,41 @@
 class Chip {
-    constructor( chipSize, chipColor, chipValue ) {
+    constructor( chipSize, chipValue ) {
         this.chipSize = chipSize;
-        this.chipColor = chipColor;
         this.chipValue = chipValue;
+    }
+
+    generate() {
+        //
+    }
+
+    mount() {
+        //
+    }
+
+    removeSequence( thisTarget ) {
+        delete thisTarget.id;
+        thisTarget.remove();
     }
 }
 
+const chipStorage = [];
 
-const gameLogic = {
-
-    chipStorage: [],
+const gameSetup = {
 
     theBody: document.getElementsByTagName( 'body' ),
 
     initialize( number, minValue, maxValue ) {
 
         for ( let i = 0; i < number; i++ ) {
-            this.chipStorage.push( new Chip(
+            chipStorage.push( new Chip(
                 1,
-                "red",
                 Math.round( Math.random( minValue ) * maxValue ) ) );
-
         }
     },
 
     setStage() {
 
-        for ( let i = 0; i < this.chipStorage.length; i++ ) {
+        for ( let i = 0; i < chipStorage.length; i++ ) {
 
             //  Get interface
             const stage = document.querySelector( '#stage-container' );
@@ -35,6 +44,11 @@ const gameLogic = {
             //  Generate random placement
             const randH = Math.min( window.innerHeight - scoreArea, Math.round( ( Math.random() * window.innerHeight ) + scoreArea ) );
             const randW = Math.min( window.innerWidth - 96, Math.round( ( Math.random() * window.innerWidth ) ) );
+
+            //  Generate random colors
+            const randR = Math.round( Math.random() * 255 );
+            const randG = Math.round( Math.random() * 255 );
+            const randB = Math.round( Math.random() * 255 );
 
             //  Create DOM elements
             const chipDiv = document.createElement( 'div' );
@@ -59,9 +73,9 @@ const gameLogic = {
                 ${randW}px;
                 border-radius: 2.5rem;
                 background-color: rgb(
-                    ${Math.round( Math.random() * 255 )},
-                    ${Math.round( Math.random() * 255 )},
-                    ${Math.round( Math.random() * 255 )});
+                    ${randR},
+                    ${randG},
+                    ${randB});
                 `;
 
             stage.appendChild( chipDiv );
@@ -74,8 +88,8 @@ const gameLogic = {
 
 const interactionBoy = {
     start() {
-        gameLogic.initialize( 25, 3, 10 );
-        gameLogic.setStage();
+        gameSetup.initialize( 25, 3, 10 );
+        gameSetup.setStage();
         this.clickyBoy();
     },
 
@@ -85,10 +99,10 @@ const interactionBoy = {
             if ( e.target.className === 'chip' && e.target.id >= 0 ) {
                 const theScore = document.querySelector( '.score' );
 
-                theScore.textContent = parseInt( theScore.textContent ) + parseInt( gameLogic.chipStorage[e.target.id].chipValue );
+                theScore.textContent = parseInt( theScore.textContent ) + parseInt( chipStorage[e.target.id].chipValue );
 
-                delete gameLogic.chipStorage[e.target.id];
-                e.target.remove();
+                chipStorage[e.target.id].removeSequence( e.target );
+
             }
         } )
     }
---
+++ practice/dayx-javascript-6/chip_game/script_v2.js
@@ -0,0 +1,113 @@
+class Chip {
+    constructor( chipId, chipSize, chipColor, chipValue, chipPosition ) {
+        this.chipId = chipId;
+        this.chipSize = chipSize;
+        this.chipValue = chipValue;
+        this.chipColorR = chipColor[0];
+        this.chipColorG = chipColor[1];
+        this.chipColorB = chipColor[2];
+        this.positionW = chipPosition[0]
+        this.positionH = chipPosition[1];
+    }
+
+    generate() {
+        //  Create DOM elements
+        const chipDiv = document.createElement( 'div' );
+        const chipP = document.createElement( 'p' );
+
+        //  Give chip an ID
+        chipP.textContent = `${this.chipId}`;
+
+        chipDiv.className = 'chip';
+        chipDiv.id = `${this.chipId}`;
+        chipDiv.style.cssText =
+            `
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 5rem;
+            height: 5rem;
+            position: absolute; 
+            top:
+            ${this.positionH}px;
+            left:
+            ${this.positionW}px;
+            border-radius: 2.5rem;
+            background-color: rgb(
+                ${this.chipColorR},
+                ${this.chipColorG},
+                ${this.chipColorB});
+            `;
+        chipDiv.appendChild( chipP );
+        return chipDiv;
+    }
+
+    mount() {
+        const stage = document.querySelector( '#stage-container' );
+        stage.appendChild( this.generate() );
+
+    }
+
+    removeSequence( thisTarget ) {
+        delete thisTarget.id;
+        thisTarget.remove();
+    }
+}
+
+const chipStorage = [];
+
+const gameSetup = {
+
+    theBody: document.getElementsByTagName( 'body' ),
+
+    initialize( number, minValue, maxValue ) {
+        //  Get interface
+        const scoreArea = document.querySelector( '#score-area' ).clientHeight;
+
+        for ( let i = 0; i < number; i++ ) {
+
+            //  Generate random placement
+            const randH = Math.min( window.innerHeight - scoreArea, Math.round( ( Math.random() * window.innerHeight ) + scoreArea ) );
+            const randW = Math.min( window.innerWidth - 96, Math.round( ( Math.random() * window.innerWidth ) ) );
+
+            //  Generate random colors
+            const randR = Math.round( Math.random() * 255 );
+            const randG = Math.round( Math.random() * 255 );
+            const randB = Math.round( Math.random() * 255 );
+
+            chipStorage.push( new Chip(
+                i, 1, [randR, randG, randB],
+                Math.round( Math.random( minValue ) * maxValue ),
+                [randW, randH] ) );
+        }
+    },
+
+    setStage() {
+        for ( let i = 0; i < chipStorage.length; i++ ) {
+            chipStorage[i].mount();
+        }
+    }
+}
+
+
+const interactionBoy = {
+    start() {
+        gameSetup.initialize( 25, 3, 10 );
+        gameSetup.setStage();
+        this.clickyBoy();
+    },
+
+    clickyBoy() {
+        document.addEventListener( 'click', ( e ) => {
+
+            if ( e.target.className === 'chip' && e.target.id >= 0 ) {
+                const theScore = document.querySelector( '.score' );
+
+                theScore.textContent = parseInt( theScore.textContent ) + parseInt( chipStorage[e.target.id].chipValue );
+
+                chipStorage[e.target.id].removeSequence( e.target );
+
+            }
+        } )
+    }
+}
